807e1d51e9a0ec864797fd83b588f2f9c46844e9
noiseterrorist <noiser999@o2.pl>
2011-08-21 17:00:14 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RorCms::Application.config.secret_token = '3eb32ff1a6013fa355c835ccf569808a88c03e11a1cc422f3d709405ae7dacb76843056b83ced6253f567ffa47f8105f087e5524bf9e7266e36f3700fbcb6567'
