62ed72e971b53c6bf43f5909b4a814f3edc49ca6
noiseterrorist <noiser999@o2.pl>
2011-08-24 22:56:36 +0200

    add tests for roles controller

--- test/functional/admin/roles_controller_test.rb
+++ test/functional/admin/roles_controller_test.rb
@@ -1,8 +1,55 @@
 require 'test_helper'
 
 class Admin::RolesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  setup do
+    @role = roles(:one)
+    @some_role = {
+      :name         => 'role_name',
+      :description  => 'some_role description'
+    }
   end
+  
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:roles)
+  end
+  
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should crete role" do
+    assert_difference('Role.count') do
+      post :create, :role => @some_role
+    end
+    
+    assert_redirected_to admin_role_path(assigns(:role))
+  end
+  
+  test "should show role" do
+    get :show, :id => @role.to_param
+    assert_response :success
+  end
+  
+  test "should get edit" do
+    get :edit, :id => @role.to_param
+    assert_response :success
+  end
+  
+  test "should update role" do
+    put :update, :id => @role.to_param, :role => @some_role
+    assert_redirected_to admin_role_path(assigns(:product))
+  end
+  
+  test "should destroy role" do
+    assert_difference('Role.count', -1) do
+      delete :destroy, :id => @role.to_param
+    end
+    
+    assert_redirected_to admin_roles_url
+  end
+  
 end
